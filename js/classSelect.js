4209da56131b9f8dd8ca03c1b284ffc49155375f
BADrie22 <badrinathshenoy22@gmail.com>
2023-12-20 13:52:07 +0530

    uno-reverse

--- js/classSelect.js
+++ js/classSelect.js
@@ -8,7 +8,7 @@ function classSelect(str) {
 }
 
 // Fetch and insert the navbar content
-fetch("./navbar/navbar.html")
+fetch("./navbar.html")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
