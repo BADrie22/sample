801319beb7390fcaf6c3fad991302bac2478d6ff
BADrie22 <badrinathshenoy22@gmail.com>
2023-12-23 15:26:58 +0530

    somehtin

--- js/classSelect.js
+++ js/classSelect.js
@@ -7,6 +7,20 @@ function classSelect(str) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    const creditInput = document.getElementById('creditInput')
+    if (creditInput) {
+        creditInput.addEventListener('input', changeVal)
+    }
+
+    const chips = document.querySelectorAll('.chipsets > div')
+    if (chips) {
+        chips.forEach((chip) => {
+            chip.addEventListener('click', function () {
+                addVal(this)
+            })
+        })
+    }
+
     const editButton = document.getElementById('editProfileBtn')
     const multiButton = document.getElementById('btnSection')
     const discardBtn = document.getElementById('discard')
@@ -30,31 +44,34 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Fetch error:', error)
         })
 
-        if(creditsBtn instanceof Element){
-
-            creditsBtn.addEventListener('click', () => {
-                fetch('./buyCredits.html')
-                    .then((response) => {
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok')
-                        }
-                        return response.text()
-                    })
-                    .then((data) => {
-                        document.getElementById('creditsContainer').innerHTML = data
-                    })
-                    .catch((error) => {
-                        console.error('Fetch error for buyCredits.html:', error)
-                    })
-            })
-        }
+    if (creditsBtn instanceof Element) {
+        creditsBtn.addEventListener('click', () => {
+            fetch('./buyCredits.html')
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok')
+                    }
+                    return response.text()
+                })
+                .then((data) => {
+                    document.getElementById('creditsContainer').innerHTML = data
+                })
+                .catch((error) => {
+                    console.error('Fetch error for buyCredits.html:', error)
+                })
+        })
+    }
 
     // Event listeners for edit and discard buttons
-    editButton.addEventListener('click', () => setEditMode(true))
-    discardBtn.addEventListener('click', () => {
-        setEditMode(false)
-        discardChanges()
-    })
+    if (editButton) {
+        editButton.addEventListener('click', () => setEditMode(true))
+    }
+    if (discardBtn) {
+        discardBtn.addEventListener('click', () => {
+            setEditMode(false)
+            discardChanges()
+        })
+    }
 
     // Function to set the edit mode
     function setEditMode(enable) {
@@ -75,3 +92,46 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 })
+
+function changeVal(event) {
+    const creditInput = document.getElementById('creditInput')
+    const balance = document.getElementById('balance')
+    const creditScore = document.getElementById('creditScore').innerText
+    const totalBalElements = document.querySelectorAll('span#totalBal')
+    const payBtn = document.getElementById('payBtn')
+
+    if (creditInput && balance && totalBalElements && payBtn) {
+        let inputValue = Number(creditInput.value)
+
+        if (isNaN(inputValue) || inputValue <= 0) {
+            creditInput.value = balance.innerText = inputValue = 0
+            
+            totalBalElements.forEach((totalBal) => {
+                totalBal.innerText = inputValue * creditScore
+            })
+            payBtn.setAttribute('disabled', 'true')
+            event.preventDefault()
+        } else {
+            payBtn.removeAttribute('disabled')
+        }
+
+        balance.innerText = inputValue
+        creditInput.value = inputValue
+
+        totalBalElements.forEach((totalBal) => {
+            totalBal.innerText = inputValue * creditScore
+        })
+    }
+}
+
+function addVal(val) {
+    const creditInput = document.getElementById('creditInput')
+    if (creditInput) {
+        let numValue = Number(val.innerText)
+
+        if (!isNaN(numValue)) {
+            creditInput.value = Number(creditInput.value) + numValue
+            changeVal()
+        }
+    }
+}
